bd799fdaa9eb5ac63bdc400a1e8131770d243a9f
chinchillapsico <coraline.colous@gmail.com>
2019-10-03 22:13:41 -0400

    xd

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  get 'articles/new'
+  post 'articles', to: 'articles#create'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
